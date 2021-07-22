a544ac0e8fe00602126cda648546ccfa88da8a5f
Nika Nasaridze <nnasa17@freeuni.edu.ge>
2021-07-22 15:51:15 +0400

    Conversations + settings UI

--- app/src/main/java/ge/nnasaridze/messengerapp/scenes/menu/MenuActivity.kt
+++ app/src/main/java/ge/nnasaridze/messengerapp/scenes/menu/MenuActivity.kt
@@ -1,12 +1,12 @@
-package ge.nnasaridze.messengerapp.scenes.conversations
+package ge.nnasaridze.messengerapp.scenes.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ge.nnasaridze.messengerapp.R
 
-class ConversationsActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_conversations)
+        setContentView(R.layout.activity_menu)
     }
 }
