97610308b3403706a7d0ae37fa7c0dea0dd04181
Nika Nasaridze <nnasa17@freeuni.edu.ge>
2021-07-23 18:48:02 +0400

    Search + chat UI + ui bugfixes
    collapsing toolbar unfinished, also needs testing

---
+++ app/src/main/java/ge/nnasaridze/messengerapp/scenes/search/SearchActivity.kt
@@ -0,0 +1,12 @@
+package ge.nnasaridze.messengerapp.scenes.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ge.nnasaridze.messengerapp.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
